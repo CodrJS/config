7dde09e4179e8bd46e61f4b44f25800319dd5975
DylanBulmer <dylan@bulmersolutions.com>
2023-04-09 11:14:33 -0400

    update node modules to be a record

--- src/config/NodeConfig.ts
+++ src/config/NodeConfig.ts
@@ -1,25 +1,30 @@
 export const NodeConfig: {
   env: "production" | "developement" | "testing";
   version: string;
-  modules: string[];
+  modules: Record<string, string>;
   yarnVersion: string;
 } = {
   env: process.env.NODE_ENV as "production" | "developement" | "testing",
   version: process.env.NODE_VERSION as string,
   modules: Object.keys(process.env)
     .filter(m => /npm_package_dependencies_/g.test(m))
-    .map(m => m.replace(/npm_package_dependencies_/g, ""))
-    .map(m =>
-      m
+    .map(m => [m, process.env[m]] as [string, string])
+    .map(m => m[0].replace(/npm_package_dependencies_/g, ""))
+    .map(m => [
+      m[0]
         .split("_")
         .map((p, idx) =>
           p === ""
             ? "@"
-            : idx === m.split("_").length - 1 && m.split("_").length !== 1
+            : idx === m[0].split("_").length - 1 && m[0].split("_").length !== 1
             ? `/${p}`
             : p,
         )
         .join(""),
-    ),
+      m[1],
+    ])
+    .reduce((acc, curr) => {
+      return (acc[curr[0]] = curr[1]);
+    }, {}),
   yarnVersion: process.env.YARN_VERSION as string,
 };
