5f71d646896de6e66f366850c6ab175c04201cbd
DylanBulmer <dylan@bulmersolutions.com>
2023-04-15 13:03:30 -0400

    update git config

--- src/config/GitConfig.ts
+++ src/config/GitConfig.ts
@@ -1,9 +1,18 @@
 export const GitConfig: {
-  branch: string;
-  commit: string;
+  ref: string;
+  commit: { sha: string; timestamp?: string };
   repo: string;
+  workflow: {
+    sha: string;
+  }
 } = {
-  branch: process.env.GIT_BRANCH as string,
-  commit: process.env.GIT_COMMIT as string,
+  ref: process.env.GIT_REF as string,
+  commit: {
+    sha: process.env.GIT_COMMIT_SHA as string,
+    timestamp: process.env.GIT_COMMIT_TIME,
+  },
   repo: process.env.GIT_REPO as string,
+  workflow: {
+    sha: process.env.GIT_WORKFLOW_SHA as string,
+  },
 };
